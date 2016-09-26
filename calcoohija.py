e84176047210cfea5cc95f2e8f0bf332896622fb
christianloos <christianloos43@gmail.com>
2016-09-26 18:41:30 +0200

    Pasado por PEP8

--- calcoohija.py
+++ calcoohija.py
@@ -4,18 +4,19 @@
 import sys
 import calcoo
 
+
 class CalculadoraHija(calcoo.Calculadora):
 
     def multiply(self, op1, op2):
         return op1 * op2
-    
+
     def divide(self, op1, op2):
         return op1 / op2
 
 if __name__ == "__main__":
     calculadora = CalculadoraHija()
 
-    try: 
+    try:
         operando1 = int(sys.argv[1])
         operando2 = int(sys.argv[3])
     except ValueError:
@@ -28,15 +29,13 @@ if __name__ == "__main__":
     elif sys.argv[2] == "multiplica":
         result = calculadora.multiply(operando1, operando2)
     elif sys.argv[2] == "divide":
-        try:        
+        try:
             result = calculadora.divide(operando1, operando2)
         except ZeroDivisionError:
             sys.exit("Division by zero is not allowed")
 
     else:
-        sys.exit('Operación sólo puede ser sumar, restar, multiplicar o dividir')
+        sys.exit('Operación sólo puede ser sumar, restar,'
+                 'multiplicar o dividir')
 
     print(result)
-
-    
-
